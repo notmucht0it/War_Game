b08caf5b3ffb26415ac285aa86268f370b7af647
notmucht0it <kevinmonahan01@gmail.com>
2023-10-30 21:49:08 -0500

    Got basic transmission working

--- war-client.py
+++ war-client.py
@@ -4,3 +4,10 @@ CompSci 520 - Homework 3
 10/30/2023
 War Client
 """
+import socket
+PORT = 4444
+
+client_socket = socket.socket()  # instantiate
+client_socket.connect(("127.0.0.1", PORT))  # connect to the server
+
+client_socket.close()  # close the connection
--- war-server.py
+++ war-server.py
@@ -4,3 +4,13 @@ CompSci 520 - Homework 3
 10/30/2023
 War Server
 """
+import socket
+PORT = 4444
+
+server_socket = socket.socket()
+server_socket.bind(("127.0.0.1", PORT))
+server_socket.listen(3)
+conn, address = server_socket.accept()  # accept new connection
+print("Connection from: " + str(address))
+
+conn.close()  # close the connection
